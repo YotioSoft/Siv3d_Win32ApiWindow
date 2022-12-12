3ebb4daa2d9a567cd6dcb186333ca5d9b3db6863
YotioSoft <ytani0323@gmail.com>
2022-12-12 18:09:45 +0900

    add getDpiDist()

--- RoundWindow/Siv3d_Win32ApiWindow/Main.cpp
+++ RoundWindow/Siv3d_Win32ApiWindow/Main.cpp
@@ -1,6 +1,14 @@
 ﻿#include <Siv3D.hpp> // OpenSiv3D v0.6.6
 #include <Siv3D/Windows/Windows.hpp>
 
+#define DPI_STANDARD	96
+
+// 現在の dpi 値を取得
+// 96dpi に対して何倍の値か？
+double getDpiDist(HWND hWnd) {
+	return (double)GetDpiForWindow(hWnd) / DPI_STANDARD;
+}
+
 void Main()
 {
 	// 標準機能で枠なしウィンドウを作成
@@ -13,15 +21,17 @@ void Main()
 	auto hWnd = static_cast<HWND>(s3d::Platform::Windows::Window::GetHWND());
 
 	// ウィンドウを角丸にする（要Win32API）
-	// まず、ウィンドウの形を示すためのリージョンを作成（大きさ640x480, 角丸100x100）
-	auto hRegion = CreateRoundRectRgn(0, 0, 640, 480, 100, 100);
+	// 現在の dpi を取得（←追加）
+	double dpi_dist = getDpiDist(hWnd);
+	// ウィンドウの形を示すためのリージョンを作成（大きさ:ウィンドウサイズ * dpiの倍数, 角丸100x100）
+	auto hRegion = CreateRoundRectRgn(0, 0, Scene::Width() * dpi_dist, Scene::Height() * dpi_dist, 100, 100);
 	// 次にウィンドウに適用
 	SetWindowRgn(hWnd, hRegion, 1);
 
 	// マウスクリックした地点の記録用
-	Point mouse_clicked;
+	Point mouse_clicked = Point{ 0, 0 };
 	while (System::Update()) {
-		// ウィンドウの移動用処理
+		// ウィンドウの移動用処理（←追加）
 		if (MouseL.down()) {
 			mouse_clicked = Cursor::Pos();
 		}
