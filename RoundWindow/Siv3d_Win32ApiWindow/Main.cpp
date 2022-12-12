9ddd650a2118c966b012f3e20fd1e7297fa606c8
YotioSoft <ytani0323@gmail.com>
2022-12-13 00:31:14 +0900

    making star window

--- RoundWindow/Siv3d_Win32ApiWindow/Main.cpp
+++ RoundWindow/Siv3d_Win32ApiWindow/Main.cpp
@@ -17,6 +17,8 @@ void Main()
 	// 背景色は白に
 	Scene::SetBackground(Color(Palette::White));
 
+	Window::Resize(300, 300);
+
 	// Windowハンドル取得
 	auto hWnd = static_cast<HWND>(s3d::Platform::Windows::Window::GetHWND());
 
@@ -24,8 +26,29 @@ void Main()
 	// 現在の dpi を取得（←追加）
 	double dpi_dist = getDpiDist(hWnd);
 	// ウィンドウの形を示すためのリージョンを作成（大きさ:ウィンドウサイズ * dpiの倍数, 角丸100x100）
-	auto hRegion = CreateRoundRectRgn(0, 0, Scene::Width() * dpi_dist, Scene::Height() * dpi_dist, 100, 100);
-	// 次にウィンドウに適用
+	//auto hRegion = CreateRoundRectRgn(0, 0, Scene::Width() * dpi_dist, Scene::Height() * dpi_dist, 100, 100);
+	// 楕円形リージョンの生成
+	// 左上x=0, 左上y=0, 右下x=Scene::Width(), 右下y=Scene::Height()
+	//auto hRegion = CreateEllipticRgn(0, 0, Scene::Width(), Scene::Height());
+	// 三角形リージョンの生成
+	/*
+	POINT pt[5];
+	pt[0].x = 0;				pt[0].y = 0;					// 頂点1: (0, 0)
+	pt[1].x = Scene::Width();	pt[1].y = 0;					// 頂点2: (Scene::Width(), 0)
+	pt[2].x = 30;				pt[2].y = Scene::Height();		// 頂点3: (30, Scene::Height())
+	auto hRegion = CreatePolygonRgn(pt, 3, ALTERNATE);			// 多角形リージョンより生成
+	*/
+	// 星型リージョンの生成
+	Console << U"{}, {}"_fmt(Scene::Width() / 2 * Math::Sin(90_deg), Scene::Height() / 2 * Math::Cos(90_deg));
+	POINT pt[5];
+	int r = Scene::Width() / 2;
+	pt[0].x = Scene::Width() / 2 + r * Math::Cos(-90_deg);	pt[0].y = Scene::Height() / 2 + r * Math::Sin(-90_deg);
+	pt[1].x = Scene::Width() / 2 + r * Math::Cos(-90_deg + 72_deg);	pt[1].y = Scene::Height() / 2 + r * Math::Sin(-90_deg + 72_deg);
+	pt[2].x = Scene::Width() / 2 + r * Math::Cos(-90_deg + 72_deg * 2);	pt[2].y = Scene::Height() / 2 + r * Math::Sin(-90_deg + 72_deg * 2);
+	pt[3].x = Scene::Width() / 2 + r * Math::Cos(-90_deg + 72_deg * 3);	pt[3].y = Scene::Height() / 2 + r * Math::Sin(-90_deg + 72_deg * 3);
+	pt[4].x = Scene::Width() / 2 + r * Math::Cos(-90_deg + 72_deg * 4);	pt[4].y = Scene::Height() / 2 + r * Math::Sin(-90_deg + 72_deg * 4);
+	auto hRegion = CreatePolygonRgn(pt, 5, ALTERNATE);			// 多角形リージョンより生成
+	// ウィンドウに適用
 	SetWindowRgn(hWnd, hRegion, 1);
 
 	// マウスクリックした地点の記録用
